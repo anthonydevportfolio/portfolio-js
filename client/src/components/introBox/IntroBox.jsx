2faeaacbd96f73d9568a7008bbf1bff3e2dc340e
anthony.griffin <anthony.griffin@workday.com>
2024-06-01 00:48:51 -0700

    deploy config

--- client/src/components/introBox/IntroBox.jsx
+++ client/src/components/introBox/IntroBox.jsx
@@ -1,7 +1,8 @@
-import './IntroBox.css';
-import '../box.css';
-import Typewriter from 'typewriter-effect';
 import { AnimatePresence, motion } from 'framer-motion';
+import Typewriter from 'typewriter-effect';
+import '../box.css';
+import './IntroBox.css';
+import { Links } from './Links';
 
 const IntroBox = ({ slideIn, subHeader }) => {
     return (
@@ -18,12 +19,14 @@ const IntroBox = ({ slideIn, subHeader }) => {
                 <motion.section initial={{ y: '10vh' }} animate={slideIn}>
                     <h2 className='subHeader'>{subHeader}</h2>
                     <p>
-                        I'm a software engineer from Washington State, currently based in McLean, VA. I create websites
-                        and applications primarily using Java and JavaScript. I have a strong passion for computer
-                        science and I'm always looking for my next adventure.
+                    I'm a software engineer from Washington State, now based in Portland, OR. 
+                    I specialize in crafting solutions using the latest tech, with strongest skills in Java and Java/TypeScript. 
+                    Software development is my passion, and I'm constantly on the lookout for my next adventure in the field.
                     </p>
+
                 </motion.section>
             </AnimatePresence>
+            <Links />
         </div>
     );
 };
