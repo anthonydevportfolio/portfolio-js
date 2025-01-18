efa1531178c400cc0248eb84466941ab5e112d82
anthony.griffin <anthony.griffin@workday.com>
2025-01-17 18:36:50 -0800

    oops

--- src/util.ts
+++
@@ -1,8 +0,0 @@
-export const MQ = {
-    mobile: '@media (max-width: 768px)',
-    desktopSmall: '@media (max-width: 1600px)',
-    desktopMedium: '@media (max-width: 1814px)',
-    desktop: '@media (min-width: 1815px)'
-};
-
-export type ValueOf<T> = T[keyof T];
